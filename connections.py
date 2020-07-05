bf9ccede273d0010182fdc2f39fa46fd985da6f9
Kunle Oshiyoye <mgen.glder@gmail.com>
2020-07-05 15:58:52 -0400

    Update to start parsing input using regexs

--- connections.py
+++ connections.py
@@ -1,3 +1,5 @@
+import re
+
 
 def hasType(type, stringToCheck):
     return stringToCheck.find(type) != -1
@@ -5,6 +7,17 @@ def hasType(type, stringToCheck):
 input = open("skimmed_input", "r")
 
 for line in input:
-    print("Substring contains something", hasType("something", line))
+    # Toss out everything that's not TCP
+    s = re.search('\{TCP\}', line)
+    if (s != None):
+        p = re.compile("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):?(\d{0,9})\s->\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):?(\d{0,9})")
+        ip1 = p.search(line).group(1)
+        port1 = p.search(line).group(2)
+        ip2 = p.search(line).group(3)
+        port2 = p.search(line).group(4)
+        print(ip1)
+        print(port1)
+        print(ip2)
+        print(port2)
 # print(input.readline())
 input.close()
