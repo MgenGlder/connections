d4fe2a490ba7dd85b61bc9f2038c9045cec408fa
Kunle Oshiyoye <mgen.glder@gmail.com>
2020-07-05 17:13:32 -0400

    Add new scan to test on, finish generating the connections via iterating

--- connections.py
+++ connections.py
@@ -10,7 +10,7 @@ def filterFunction(variable):
     else:
         return False
 
-def determineLabel(stringToCheck):
+def determineConnectionType(stringToCheck):
     c = stringToCheck.find("cnc") != -1
     o = stringToCheck.find("other") != -1
     i = stringToCheck.find("infection") != -1
@@ -32,21 +32,62 @@ def determineLabel(stringToCheck):
         else:
             return "unknown"
 
-input = open("skimmed_input", "r")
+class ConnectionInfo(object):
+    connection_types: []
+    connection_label: ""
+    addresses: []
+    ports: []
+    def __init__(self, connection_types, connection_label, addresses, ports):
+        self.connection_label = connection_label
+        self.connection_types = connection_types
+        self.addresses = addresses
+        self.ports = ports
 
-for line in input:
-    # Toss out everything that's not TCP
-    s = re.search('\{TCP\}', line)
-    if (s != None):
-        p = re.compile("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):?(\d{0,9})\s->\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):?(\d{0,9})")
-        ip1 = p.search(line).group(1)
-        port1 = p.search(line).group(2)
-        ip2 = p.search(line).group(3)
-        port2 = p.search(line).group(4)
-        print(ip1)
-        print(port1)
-        print(ip2)
-        print(port2)
-        print("Label is.. ", determineLabel(line))
-# print(input.readline())
-input.close()
+
+
+def generateConnectionsDictionary(input):
+    items = dict()
+    for line in input:
+        # Toss out everything that's not TCP
+        # Also should check for empty lines
+        s = re.search('\{TCP\}', line)
+        if (s != None):
+            p = re.compile("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):?(\d{0,9})\s->\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):?(\d{0,9})")
+            ip1 = p.search(line).group(1)
+            port1 = p.search(line).group(2)
+            ip2 = p.search(line).group(3)
+            port2 = p.search(line).group(4)
+            connectionType = determineConnectionType(line)
+            print(ip1)
+            print(port1)
+            print(ip2)
+            print(port2)
+            print("Label is.. ", determineConnectionType(line))
+            addressTupleVariation1 = (ip1, ip2)
+            addressTupleVariation2 = (ip2, ip1)
+            if (addressTupleVariation1 in items):
+                print("found it")
+                types = items[addressTupleVariation1].connection_types
+                if (connectionType not in types):
+                    print("Didn't have this connection type, so added it: ", connectionType)
+                    types.append(connectionType)
+            elif (addressTupleVariation2 in items):
+                types = items[addressTupleVariation2].connection_types
+                print("found it")
+                if (connectionType not in types):
+                    print("Didn't have this connection type, so added it: ", connectionType)
+                    types.append(connectionType)
+            else:
+                print("didn't find it, so adding")
+                items[addressTupleVariation1] = ConnectionInfo([connectionType], None, [ip1, ip2], [port1, port2])
+    return items
+
+
+
+def main():
+    input = open("bad_scan", "r")
+    generateConnectionsDictionary(input)
+    input.close()
+
+if __name__ == "__main__":
+    main()
